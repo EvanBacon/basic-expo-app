ba6fbe61a4937eb0b26bc39de128f674e18897a9
evanbacon <bacon@expo.io>
2022-04-11 13:19:12 -0500

    Added a change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello World! 2</Text>
       <StatusBar style="auto" />
     </View>
   );
